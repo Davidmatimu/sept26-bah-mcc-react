d36b5e0f86005b4f3cef1649f250840952b78533
David <dmatimu@udel.edu>
2022-11-01 12:07:36 -0400

    Update setupProxy.js
    added auth and data services Ip addresses and ports from k8

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://localhost:8080/' }));
-    app.use(proxy('/account', { target: 'http://localhost:8081/' })); 
+    app.use(proxy('/api', { target: 'http://10.99.193.13:8080/' }));
+    app.use(proxy('/account', { target: 'http://10.105.59.1:8081/' })); 
 	
 	/*app.use(proxy('/api', { target: 'http://172.18.0.2:8080/' }));
 	app.use(proxy('/account', { target: 'http://172.18.0.3:8081/' }));
